e6b8c6f648c1e7372593af9224f70610ce28c3df
unknown <ngochai06122002@gmail.com>
2024-03-17 22:40:48 +0700

    Fix tag

--- routes/tagRoutes.js
+++ routes/tagRoutes.js
@@ -4,10 +4,10 @@ const middlewares = require('../middlewares')
 const router = express.Router();
 
 
-router.post('/addTag',middlewares.authenticateToken,middlewares.filter(['Admin']), controller.tagController.addTAg);
+router.post('/addTag',middlewares.authenticateToken, controller.tagController.addTAg);
 router.get('/allTag',middlewares.authenticateToken, controller.tagController.getAllTags);
-router.get('/:tagId',middlewares.authenticateToken,middlewares.filter(['Admin']), controller.tagController.getTagById);
-router.delete('/:tagId',middlewares.authenticateToken,middlewares.filter(['Admin']),controller.tagController.deleteTagById);
+router.get('/:tagId',middlewares.authenticateToken, controller.tagController.getTagById);
+router.delete('/:tagId',middlewares.authenticateToken,controller.tagController.deleteTagById);
 
 
 module.exports = router;
