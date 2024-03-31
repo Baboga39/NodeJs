dc73799eec59a2c848701e2c690bd6fd1d0e3be9
unknown <ngochai06122002@gmail.com>
2024-03-31 21:25:49 +0700

    Socket

--- index.js
+++ index.js
@@ -6,27 +6,66 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const http = require('http');
-const { Server } = require('socket.io'); 
+const { Server } = require("socket.io");
 
 const app = express();
-// const corsOptions = {
-//   origin: 'http://localhost:3000',
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   credentials: true, // 
-// };
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+let users = [];
 
-app.use(cors());
+const addUser = (id, socketId) => {
+  const index = users.findIndex((u) => u.id === id);
+  if (index === -1) {
+    users.push({ id, socketId });
+    return true;
+  }
+  users[index].socketId = socketId;
+  return true;
+};
 
-io.on('connection', (socket) => {
-  console.log('User connected');
-  
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (id) => {
+  return users.find((user) => user.id === id);
+};
+
+io.on("connection", (socket) => {
+  socket.on("addUser", ({ userId }) => {
+    console.log("Add user in socket Success " + userId + " - ", socket.id);
+    if (addUser(userId, socket.id)) {
+      io.emit("getUsers", users);
+    }
+  });
+
+  socket.on("interaction", ({ fromUser, toUser }) => {
+    console.log(`User ${fromUser} interacts with user ${toUser}`);
+    if (fromUser === toUser) {
+      console.log("The same user is interacting with itself. No need to send socket.");
+      return; 
+    }
+    const recipientSocket = getUser(toUser)?.socketId;
+    if (recipientSocket) {
+      console.log("User receiver is online.");
+      io.to(recipientSocket).emit("notification", { fromUser });
+    }
+    else{ console.log("User receiver is not online.");
+  }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnect ", socket.id);
+    removeUser(socket.id);
+    io.emit("getUsers", users);
   });
 });
 
+app.use(cors());
 app.use(bodyParser.json());
 routes(app);
 
@@ -39,6 +78,6 @@ mongoose.connect(`${process.env.Mongo_DB}`, {
 });
 
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('Server is running on port ' + port);
 });
