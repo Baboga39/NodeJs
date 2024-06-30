1f02267c88afe98499671ab6a6a5be24dd264b5a
unknown <ngochai06122002@gmail.com>
2024-06-30 21:46:08 +0700

    Update performence ispermission last

--- services/blogService.js
+++ services/blogService.js
@@ -271,7 +271,6 @@ class BlogService{
                     const category = await Category.findById(category_id);
                     if(category && category.users){
                         const isPermission = category.users.some(user => user._id.equals(userId));
-                        if((category.status === 'Publish' && isPermission) || (category.status === 'Private' && isPermission))
                         updateFields = {
                             isPermission: true
                         }
