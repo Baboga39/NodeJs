1a356a961c1091661776ff9d3451ea2516f5dde2
unknown <ngochai06122002@gmail.com>
2024-07-08 21:22:28 +0700

    Update code

--- chatBox/gemini-pro.js
+++ chatBox/gemini-pro.js
@@ -13,7 +13,7 @@ const run = async (req,res) => {
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
     const result = await model.generateContent(prompt);
-    const response = await result.response;
+    const response = result.response;
     const text = response.text();
     console.log("Generative model generated");
     console.log('--------------------------------------------------------------------------------------------------------------------');
