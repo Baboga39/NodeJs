c8f7ae30b14ba48f061845107a317a85210a4c21
unknown <ngochai06122002@gmail.com>
2024-07-11 20:11:13 +0700

    Update feed

--- services/blogService.js
+++ services/blogService.js
@@ -725,112 +725,6 @@ class BlogService{
     
 
 // 3s
-// static listBlogInFeed = async (authenticatedUser, pageIndex) => {
-//     try {
-//         const access = new Access({ user: authenticatedUser._id });
-//         const today = new Date();
-//         today.setHours(0, 0, 0, 0);
-//         const tomorrow = new Date(today);
-//         tomorrow.setDate(today.getDate() + 1);
-
-//         const checkAccess = await Access.findOne({
-//             user: authenticatedUser._id,
-//             createdAt: { $gte: today, $lt: tomorrow }
-//         });
-
-//         if (!checkAccess) {
-//             await access.save();
-//         }
-
-//         const pageSize = 6;
-//         const startIndex = (pageIndex - 1) * pageSize;
-//         const endIndex = pageIndex * pageSize;
-//         const uniquePostIds = new Set();
-//         const listBlog = [];
-
-//         const categories = await Category.find({ users: authenticatedUser._id });
-//         const follow = await followModel.findOne({ user: authenticatedUser._id });
-//         const users = follow ? follow.following : [];
-
-//         const allPostsPromises = categories.map(category => 
-//             Blog.find({ category: category._id, status: 'Published', isApproved: false })
-//                 .sort({ createdAt: -1 })
-//                 .populate('tags')
-//                 .populate('user')
-//                 .populate('category')
-//                 .exec()
-//                 .then(async query => {
-//                     let posts = await this.findAndUpdateLikeAndSave(query, authenticatedUser._id);
-//                     return this.findAndUpdatePermissions(posts, authenticatedUser._id);
-//                 })
-//         );
-
-//         const followPostsPromises = users.map(user => 
-//             Blog.find({ user: user._id, status: 'Published', isApproved: false })
-//                 .sort({ createdAt: -1 })
-//                 .populate('tags')
-//                 .populate('user')
-//                 .populate('category')
-//                 .exec()
-//                 .then(async query => {
-//                     let posts = await this.findAndUpdateLikeAndSave(query, authenticatedUser._id);
-//                     return this.findAndUpdatePermissions(posts, authenticatedUser._id);
-//                 })
-//         );
-
-//         const sharePostsPromises = users.map(async user => {
-//             const shareBlog = await Share.findOne({ user: user._id }).exec();
-//             if (shareBlog && shareBlog.listBlog) {
-//                 const blogFindShare = shareBlog.listBlog;
-//                 let posts = await this.findAndUpdateLikeAndSave(blogFindShare, authenticatedUser._id);
-//                 posts = await this.findAndUpdatePermissions(posts, authenticatedUser._id);
-
-//                 const UserFind = await User.findById(user._id).exec();
-//                 posts.forEach(post => {
-//                     post.isShare = true;
-//                     post.shareBy = UserFind;
-//                 });
-
-//                 return posts;
-//             }
-//             return [];
-//         });
-
-//         const [allPostsResults, followPostsResults, sharePostsResults] = await Promise.all([
-//             Promise.all(allPostsPromises),
-//             Promise.all(followPostsPromises),
-//             Promise.all(sharePostsPromises)
-//         ]);
-
-//         const allPosts = allPostsResults.flat();
-//         const followPosts = followPostsResults.flat();
-//         const sharePosts = sharePostsResults.flat();
-
-//         [...allPosts, ...followPosts, ...sharePosts].forEach(post => {
-//             if (!uniquePostIds.has(post._id)) {
-//                 listBlog.push(post);
-//                 uniquePostIds.add(post._id);
-//             }
-//         });
-
-//         listBlog.sort((a, b) => {
-//             if (a.createdAt > b.createdAt) return -1;
-//             if (a.createdAt < b.createdAt) return 1;
-//             if (a.updatedAt > b.updatedAt) return -1;
-//             if (a.updatedAt < b.updatedAt) return 1;
-//             return 0;
-//         });
-
-//         const size = Math.ceil(listBlog.length / pageSize);
-//         const paginatedPosts = listBlog.slice(startIndex, endIndex);
-
-//         return { size, posts: paginatedPosts };
-//     } catch (error) {
-//         console.error("Error fetching most active posts:", error);
-//         return null;
-//     }
-// };
-
 static listBlogInFeed = async (authenticatedUser, pageIndex) => {
     try {
         const access = new Access({ user: authenticatedUser._id });
@@ -854,71 +748,71 @@ static listBlogInFeed = async (authenticatedUser, pageIndex) => {
         const uniquePostIds = new Set();
         const listBlog = [];
 
-        // Fetch categories and follow users concurrently
-        const [categories, follow] = await Promise.all([
-            Category.find({ users: authenticatedUser._id }).exec(),
-            followModel.findOne({ user: authenticatedUser._id }).exec()
-        ]);
-
+        const categories = await Category.find({ users: authenticatedUser._id });
+        const follow = await followModel.findOne({ user: authenticatedUser._id });
         const users = follow ? follow.following : [];
 
-        // Combine all post queries into one array
-        const postQueries = [
-            ...categories.map(category => 
-                Blog.find({ category: category._id, status: 'Published', isApproved: false })
-                    .sort({ createdAt: -1 })
-                    .populate('tags')
-                    .populate('user')
-                    .populate('category')
-                    .exec()
-            ),
-            ...users.map(user => 
-                Blog.find({ user: user._id, status: 'Published', isApproved: false })
-                    .sort({ createdAt: -1 })
-                    .populate('tags')
-                    .populate('user')
-                    .populate('category')
-                    .exec()
-            ),
-            ...users.map(user => 
-                Share.findOne({ user: user._id }).exec()
-                    .then(shareBlog => {
-                        if (shareBlog && shareBlog.listBlog) {
-                            return Blog.find({ _id: { $in: shareBlog.listBlog } })
-                                .populate('tags')
-                                .populate('user')
-                                .populate('category')
-                                .exec()
-                                .then(posts => {
-                                    posts.forEach(post => {
-                                        post.isShare = true;
-                                        post.shareBy = user;
-                                    });
-                                    return posts;
-                                });
-                        }
-                        return [];
-                    })
-            )
-        ];
+        const allPostsPromises = categories.map(category => 
+            Blog.find({ category: category._id, status: 'Published', isApproved: false })
+                .sort({ createdAt: -1 })
+                .populate('tags')
+                .populate('user')
+                .populate('category')
+                .exec()
+                .then(async query => {
+                    let posts = await this.findAndUpdateLikeAndSave(query, authenticatedUser._id);
+                    return this.findAndUpdatePermissions(posts, authenticatedUser._id);
+                })
+        );
 
-        // Fetch and process posts concurrently
-        const postsResults = await Promise.all(postQueries);
-        const posts = postsResults.flat();
+        const followPostsPromises = users.map(user => 
+            Blog.find({ user: user._id, status: 'Published', isApproved: false })
+                .sort({ createdAt: -1 })
+                .populate('tags')
+                .populate('user')
+                .populate('category')
+                .exec()
+                .then(async query => {
+                    let posts = await this.findAndUpdateLikeAndSave(query, authenticatedUser._id);
+                    return this.findAndUpdatePermissions(posts, authenticatedUser._id);
+                })
+        );
 
-        // Process posts
-        let processedPosts = await this.findAndUpdateLikeAndSave(posts, authenticatedUser._id);
-        processedPosts = await this.findAndUpdatePermissions(processedPosts, authenticatedUser._id);
+        const sharePostsPromises = users.map(async user => {
+            const shareBlog = await Share.findOne({ user: user._id }).exec();
+            if (shareBlog && shareBlog.listBlog) {
+                const blogFindShare = shareBlog.listBlog;
+                let posts = await this.findAndUpdateLikeAndSave(blogFindShare, authenticatedUser._id);
+                posts = await this.findAndUpdatePermissions(posts, authenticatedUser._id);
 
-        // Combine and filter posts
-        processedPosts.forEach(post => {
+                const UserFind = await User.findById(user._id).exec();
+                posts.forEach(post => {
+                    post.isShare = true;
+                    post.shareBy = UserFind;
+                });
+
+                return posts;
+            }
+            return [];
+        });
+
+        const [allPostsResults, followPostsResults, sharePostsResults] = await Promise.all([
+            Promise.all(allPostsPromises),
+            Promise.all(followPostsPromises),
+            Promise.all(sharePostsPromises)
+        ]);
+
+        const allPosts = allPostsResults.flat();
+        const followPosts = followPostsResults.flat();
+        const sharePosts = sharePostsResults.flat();
+
+        [...allPosts, ...followPosts, ...sharePosts].forEach(post => {
             if (!uniquePostIds.has(post._id)) {
                 listBlog.push(post);
                 uniquePostIds.add(post._id);
             }
         });
 
-        // Sort posts by createdAt and updatedAt
         listBlog.sort((a, b) => {
             if (a.createdAt > b.createdAt) return -1;
             if (a.createdAt < b.createdAt) return 1;
@@ -936,6 +830,7 @@ static listBlogInFeed = async (authenticatedUser, pageIndex) => {
         return null;
     }
 };
+
 
 
     static listBlogShareByUSer = async (authenticatedUser, userId) => {
